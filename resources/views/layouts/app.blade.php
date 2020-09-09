6d4a4b034ed8e04aa5993952ac14a7f370610582
AndreyKourov <andreykourov@mail.ru>
2020-09-09 21:38:43 +0600

    навбар

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,27 +24,54 @@
     </script>
 </head>
 <body>
-    <div id="app">
+    
+     <div class="container-fluid">
+        <div class="row">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a href="{{url('topic')}}" class="nav-link {{$page === 'Main page' ? 'active' : ''}}">Main page</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{url('block/create')}}" class="nav-link {{$page === 'Forms' ? 'active' : ''}}">Content control</a>
+                    </li>
+                </ui>
+            </div>
+            </nav>
+        </div>
+     </div>       
+
+    <!--
+        <div id="app">
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
                 <div class="navbar-header">
 
-                    <!-- Collapsed Hamburger -->
+                     Collapsed Hamburger 
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
                         <span class="sr-only">Toggle Navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-
-                    <!-- Branding Image 
+                    
+                     Branding Image 
                     <a class="navbar-brand" href="{{ url('/') }}">
                         {{ config('app.name', 'Laravel') }}
-                    </a> -->
+                    </a>
+                    -->
+                    <!--
                 </div>
+                
+
                 {{ $page = null }}
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
+                    <!--
                     <ul class="nav navbar-nav">
                         <li class="nav-item">
                             <a href="{{url('topic')}}" class="nav-link {{$page === 'Main page' ? 'active' : ''}}">Main
@@ -57,8 +84,10 @@
                     </ul>
 
                     <!-- Right Side Of Navbar -->
+                    <!--
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
+                        <!--
                         @if (Auth::guest())
                             <li><a href="{{ url('/login') }}">Login</a></li>
                             <li><a href="{{ url('/register') }}">Register</a></li>
@@ -100,8 +129,12 @@
             </div>
         </div>
     @show
+    -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
-    <!-- Scripts -->
+    <!-- Scripts
     <script src="{{ asset('/js/app.js') }}"></script>
     <script
         src="https://code.jquery.com/jquery-3.5.1.min.js"
@@ -109,5 +142,6 @@
         crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    -->
 </body>
 </html>
